9578a4fec0c4388ae0cb4076975bd960330fbac7
senziai20 <isenzia@yahoo.com>
2023-11-17 19:37:16 +0300

    add  listing retrieve function_views and path_urls with <pk>

--- real_estate/real_estate/urls.py
+++ real_estate/real_estate/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from listings.views import listing_list
+from listings.views import listing_list, listing_retrieve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', listing_list )
-
+    path('', listing_list ),
+    path('listings/<pk>/', listing_retrieve)
 ]
