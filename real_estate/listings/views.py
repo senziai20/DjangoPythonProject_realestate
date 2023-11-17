581e347bdf7d51a15837921411c55023c4e18b8f
senziai20 <isenzia@yahoo.com>
2023-11-17 16:48:56 +0300

    modified: listings/views.py  'listing_list views'

--- real_estate/listings/views.py
+++ real_estate/listings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Listing
 
 # Create your views here.
+
+# CRUD - create, retrieve, update, delete, list
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    context = {
+        "listings":listings
+    }
+    return render(request, "listings.html", context)
